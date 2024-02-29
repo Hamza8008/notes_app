c490920f1931957998446d5929e9029013e2e775
Hamza <alhnawyh38@gmial.com>
2024-02-29 15:28:07 +0200

    add_app_bar

---
+++ lib/Widgets/custom_app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp/Widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 100,
+        ),
+        Text(
+          'Notes',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+        ),
+        Spacer(
+          flex: 2,
+        ),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
