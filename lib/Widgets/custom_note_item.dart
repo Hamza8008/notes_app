e9208b408140e951de5434a8f60791075f4aa36c
Hamza <alhnawyh38@gmial.com>
2024-03-17 19:24:42 +0200

    create ediit note view

--- lib/Widgets/custom_note_item.dart
+++ lib/Widgets/custom_note_item.dart
@@ -1,61 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/Views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        color: Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                'Build Your Career With Tharwat Samy',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Build Your Career With Tharwat Samy',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(.6),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                ),
+              ),
+              title: Text(
+                'Fluter Tips',
                 style: TextStyle(
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May21,2024',
+                style: TextStyle(
                   color: Colors.black.withOpacity(.6),
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
-              ),
-            ),
-            title: Text(
-              'Fluter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21,2024',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.6),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
