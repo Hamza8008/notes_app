58ee077a0e9575e856f2823076a7fd3785d84534
Hamza <alhnawyh38@gmial.com>
2024-03-19 21:22:21 +0200

    rigister adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:noteapp/Models/Note_Model.dart';
 import 'package:noteapp/Views/Notes_View.dart';
 import 'package:noteapp/constansts.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
